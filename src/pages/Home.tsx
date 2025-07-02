090b572d7fef16ee741d28ba8f268419b9cfa1ee
Mohamad Bassel <mhd.bassel.a70@gmail.com>
2025-07-02 18:51:00 +0800

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -122,7 +122,7 @@ const Home = () => {
               <span className="hero-top">Custom Designs, Custom Coded</span>
               <span>
                 <h1 className="color-white hero-title font-XL">
-                  Small Business Web Designe
+                  Small Business Web Designer
                 </h1>
               </span>
               <div className="hero-text-container">
